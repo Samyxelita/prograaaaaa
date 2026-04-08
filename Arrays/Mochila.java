7d9a32b9c975b48b81df7689a1a6efbbb4150100
Samuel Herrera <samuel.herrera@ct.uneatlantico.es>
2026-04-08 11:44:02 +0200

    Arrays carpeta y primer ejercicio

---
+++ Arrays/Mochila.java
@@ -0,0 +1,8 @@
+public class Mochila {
+    public static void main (String[] args){
+        String[] articulos = {"Comida", "Cuadernos", "Laptop"};
+
+        System.out.println("En mi mochila llevo " + articulos[1] );
+    }
+
+}
