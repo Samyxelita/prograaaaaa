b552f6076a0c0318a7cf88ba7f56f4576e6e155c
Samuel Herrera <samuel.herrera@ct.uneatlantico.es>
2026-04-10 13:25:12 +0200

    Primer programa con arrays :D

--- Arrays/Notas.java
+++ Arrays/Notas.java
@@ -1,12 +1,20 @@
 public class Notas {
     public static void main (String [] args){
-        int[] notas = {10, 8, 7, 9, 6};
-        int suma = 0;
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
 
-        for (int i = 0; i < notas.length; i++){
-            System.out.println("Nota registrada :" + notas[i]);
-            suma = suma + notas[i];
+        for (int i = 0; i < numeros.length; i ++) {
+            if (numeros[i] % 2 == 0 ) {
+                System.out.println("El numero " + numeros[i] + " es par");
+
+            }
+            else {
+                System.out.println("El numero " + numeros[i] + " es impar");
+
+
+            }
+
 
         }
+        
     }
 }
