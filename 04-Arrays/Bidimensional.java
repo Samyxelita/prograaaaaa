b2895db8adbfd17f27a16d0c69fb17f0b3d0cab3
Samuel Herrera <samuel.herrera@ct.uneatlantico.es>
2026-04-20 13:31:22 +0200

    fix: ahora si ya todo esta ordenado

---
+++ 04-Arrays/Bidimensional.java
@@ -0,0 +1,14 @@
+public class Notas {
+    public static void main (String [] args){
+        int matriz [][] = new int[2][2];
+
+        matriz[0][1] = 7;
+        matriz[1][0] = 3;
+
+        int suma =  matriz[0][1] +   matriz[1][0];
+
+           System.out.println(suma);
+
+
+    }
+}
