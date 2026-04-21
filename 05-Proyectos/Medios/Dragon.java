63b0096c27518394e6ab2d1bdfb77a7585fdff07
Samuel Herrera <samuel.herrera@ct.uneatlantico.es>
2026-04-21 11:04:17 +0200

    Dragon terminado primera Version con diagrama de flujo

--- 05-Proyectos/Medios/Dragon.java
+++ 05-Proyectos/Medios/Dragon.java
@@ -5,46 +5,53 @@ public class Dragon {
     public static void main(String[] args) {
         Random generador = new Random();
         Scanner lector = new Scanner(System.in);
-
         int vidaDragon = 100;
         int vidaHeroe = 100;
         int pociones = 3;
         int opcion;
-        int dañoHeroe = generador.nextInt(16) + 10;
-        int dañoDragon = generador.nextInt(16) + 15;
+        
 
         while (vidaDragon > 0 && vidaHeroe > 0) {
+            int dañoHeroe = generador.nextInt(16) + 15;
+            int dañoDragon = generador.nextInt(16) + 15;
             System.out.println("Elige que deseas hacer 1.Atacar 2.Curarte ");
             opcion = lector.nextInt();
-            vidaHeroe = vidaHeroe - dañoDragon;
-            if (opcion == 1) {
+            
+              if (opcion == 1) {
 
                 vidaDragon = vidaDragon - dañoHeroe;
 
-                System.out.println("Vida restante del dragon:" + vidaDragon);
-                System.out.println("Vida restante del heroe:" + vidaHeroe);
+                System.out.println("Le has hecho:" + dañoHeroe + " de dano!!");
+                
+                if(vidaDragon > 0){
+                 vidaHeroe = vidaHeroe - dañoDragon;
+                 System.out.println("El dragon te ha hecho:" + dañoDragon + " de dano!!");
+                 
+            }
+            else {
+                break;
+            }
 
-            } else if (opcion == 2) {
-                if (pociones > 0) {
+            } else if (opcion == 2 && pociones > 0) {
                     vidaHeroe = vidaHeroe + 20;
                     pociones = pociones - 1;
                     System.out.println("Curado! Vida restante: " + vidaHeroe);
                     System.out.println("Vida restante del dragon:" + vidaDragon);
-
-                }
-
+}
+                
                 else {
                     System.out.println("No tienes mas pociones ");
 
                 }
-
-            }
-               System.out.println("HEROE : " + vidaHeroe + " Dragon: " + vidaDragon + "Pociones restantes " + pociones);
-
+            
+            
+            
+            
+               System.out.println("HEROE : " + vidaHeroe + "|| Dragon: " + vidaDragon + " ||Pociones restantes " + pociones);
 
         }
         if (vidaDragon <= 0) {
-            System.out.println("¡Has derrotado al dragón y salvado el reino!");
+            System.out.println("¡Has derrotado al dragón y salvado el reino! Te has quedado con : " + vidaHeroe + " de vida");
 
         } else if (vidaHeroe <= 0) {
             System.out.println("Has muerto... el reino arde en llamas.");
@@ -53,5 +60,3 @@ public class Dragon {
     }
 
 }
-
-
