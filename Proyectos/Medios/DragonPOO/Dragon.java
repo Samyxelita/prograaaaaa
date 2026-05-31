7f049f3ff02a71f817cba045240a231054466d90
samyx <samyxelita@gmail.com>
2026-05-31 13:00:52 +0200

    feat: Clase dragon terminada

--- Proyectos/Medios/DragonPOO/Dragon.java
+++ Proyectos/Medios/DragonPOO/Dragon.java
@@ -2,4 +2,36 @@ package DragonPOO;
 
 public class Dragon {
 
+    private int vida;
+    private String nombre;
+
+    public Dragon(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void contraAtaque(Heroe heroe) {
+        heroe.setVida(-10);
+    }
+
+    public void celebra(Heroe heroe) {
+        System.out.println("Soy el dragon " + nombre + "y mate a " + heroe.getNombre());
+    }
+
+    public void setVida(int vida) {
+        if (vida < 0) {
+            this.vida = 0;
+        } else {
+            this.vida += vida;
+        }
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
 }
