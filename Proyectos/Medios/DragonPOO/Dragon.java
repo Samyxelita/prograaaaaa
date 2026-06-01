3394338e49c272d7b411b1164d9b31da3d26abb1
samyx <samyxelita@gmail.com>
2026-06-01 16:42:24 +0200

    refactor: dejamos clase Dragon limpia y observamos como queda de legible y facil de entender

--- Proyectos/Medios/DragonPOO/Dragon.java
+++ Proyectos/Medios/DragonPOO/Dragon.java
@@ -1,37 +1,13 @@
 package DragonPOO;
 
-public class Dragon {
-
-    private int vida;
-    private String nombre;
+public class Dragon extends Personaje {
 
     public Dragon(String nombre, int vida) {
-        this.nombre = nombre;
-        this.vida = vida;
-    }
-
-    public boolean estaVivo() {
-        return vida > 0;
+        super(nombre, vida);
     }
 
     public void contraAtaque(Heroe heroe) {
         heroe.recibirDano(10);
-    }
-
-    public void celebra(Heroe heroe) {
-        System.out.println("Soy el dragon " + nombre + "y mate a " + heroe.getNombre());
-    }
-
-    public void recibirDano(int danoRecibido) {
-        this.vida = Math.max(0, this.vida - danoRecibido);
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getVida() {
-        return vida;
     }
 
 }
