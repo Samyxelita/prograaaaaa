4b80185fc7189a5399ee4f33cafb08621c56ec20
samyx <samyxelita@gmail.com>
2026-06-01 16:41:17 +0200

    refactor : Creamos la clase padre 1. Extraemos los atributos que se repiten en la clase heroe y dragon 2. Extraemos los metodos que se repiten en cada clase

---
+++ Proyectos/Medios/DragonPOO/Personaje.java
@@ -0,0 +1,32 @@
+package DragonPOO;
+
+public class Personaje {
+    protected int vida;
+    protected String nombre;
+
+    public Personaje(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void recibirDano(int danoRecibido) {
+        this.vida = Math.max(0, this.vida - danoRecibido);
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void celebra(Personaje enemigo) {
+        System.out.println("Soy el personaje " + nombre + " y he matado a " + enemigo.getNombre());
+    }
+
+}
